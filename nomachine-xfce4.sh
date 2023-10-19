eb33709c70d27a73cfa17b325608de8da2eac3fb
MrBY510KONFACEBOOK <134196987+MrBY510KONFACEBOOK@users.noreply.github.com>
2023-10-19 18:52:11 +0100

    Update nomachine-xfce4.sh

--- nomachine-xfce4.sh
+++ nomachine-xfce4.sh
@@ -2,23 +2,9 @@ wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/ma
 chmod +x ng.sh
 ./ng.sh
 
-
-function goto
-{
-    label=$1
-    cd 
-    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
-          grep -v ':$')
-    eval "$cmd"
-    exit
-}
-
-: ngrok
-clear
 echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
 ./ngrok authtoken 2Q62sciZHvRunnnnVbZwLWJkND3_KJpgXdqYuzmMRnDf9VEt 
 
-clear
 echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
 echo "======================="
 echo "choose ngrok region (for better connection)."
@@ -32,9 +18,10 @@ echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 ./ngrok tcp --region us 4000 &>/dev/null &
 sleep 1
-if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!"; fi
+
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:xfce4
-clear
+
 echo "NoMachine: https://www.nomachine.com/download"
 echo Done! NoMachine Information:
 echo IP Address:
@@ -42,4 +29,16 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo User: user
 echo Passwd: 123456
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
-seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
+
+seq 1 43200 | while read i; do
+    echo -en "\r Running .     $i s /43200 s"; sleep 0.1;
+    echo -en "\r Running ..    $i s /43200 s"; sleep 0.1;
+    echo -en "\r Running ...   $i s /43200 s"; sleep 0.1;
+    echo -en "\r Running ....  $i s /43200 s"; sleep 0.1;
+    echo -en "\r Running ..... $i s /43200 s"; sleep 0.1;
+    echo -en "\r Running     . $i s /43200 s"; sleep 0.1;
+    echo -en "\r Running  .... $i s /43200 s"; sleep 0.1;
+    echo -en "\r Running   ... $i s /43200 s"; sleep 0.1;
+    echo -en "\r Running    .. $i s /43200 s"; sleep 0.1;
+    echo -en "\r Running     . $i s /43200 s"; sleep 0.1;
+done
