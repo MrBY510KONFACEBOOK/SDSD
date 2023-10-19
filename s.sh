9a10bc03795e721d6befc35fc13a61b6b21187b9
MrBY510KONFACEBOOK <134196987+MrBY510KONFACEBOOK@users.noreply.github.com>
2023-10-19 09:20:27 +0100

    Update s.sh

--- s.sh
+++ s.sh
@@ -3,7 +3,7 @@ pip install gdown
 
 # تحميل الملف الأول وفك الضغط
 gdown https://drive.google.com/uc?id=1IuwwbU-AfsH9moZcbumPtCrLo-jiDmO1
-unzip whatsapp-web-project-NEW_TIMESTAMP.zip
+unzip whatsapp-web-project-20231019T080437Z-001.zip
 
 # التنقل إلى المجلد الجديد
 cd whatsapp-web-project
