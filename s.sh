ba0153ccf40777b2adbbc1f24fc499178a207320
MrBY510KONFACEBOOK <134196987+MrBY510KONFACEBOOK@users.noreply.github.com>
2023-10-19 01:47:23 +0100

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,7 +1,8 @@
-gdown 1EpbQ6JjZdhwGtN4msg1bNrMl3JMVuurW
+pip install gdown
+sudo gdown 1EpbQ6JjZdhwGtN4msg1bNrMl3JMVuurW
 unzip  whatsapp-web-project-20231018T195854Z-001.zip
 cd whatsapp-web-project
-gdown 1QZIo7pd2d67Qb59j33cn3URAM-i5fRxE
+sudo gdown 1QZIo7pd2d67Qb59j33cn3URAM-i5fRxE
 bash i.sh
 npm install qrcode-terminal whatsapp-web.js translate-google
 node index.js
