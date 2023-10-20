7a0376bc4e2ac168565af4faf6ed2323df169173
MrBY510KONFACEBOOK <134196987+MrBY510KONFACEBOOK@users.noreply.github.com>
2023-10-20 10:11:32 +0100

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,12 @@
+pip install gdown
+
+# تحميل الملف الأول وفك الضغط
+gdown https://drive.google.com/uc?id=1IuwwbU-AfsH9moZcbumPtCrLo-jiDmO1
+unzip whatsapp-web-project-20231019T080437Z-001.zip
+
+# التنقل إلى المجلد الجديد
+cd whatsapp-web-project
+npm install qrcode-terminal whatsapp-web.js translate-google
+rm index.js
+wget 
+node .
