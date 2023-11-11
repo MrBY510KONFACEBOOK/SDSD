b2b46fc2e572bf059620c31db21b15920c54856f
MrBY510KONFACEBOOK <134196987+MrBY510KONFACEBOOK@users.noreply.github.com>
2023-11-11 16:34:38 +0100

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,21 +1,6 @@
-# تثبيت أداة gdown
-pip install gdown
-
-# تحميل الملف الأول وفك الضغط
-gdown https://drive.google.com/uc?id=1IuwwbU-AfsH9moZcbumPtCrLo-jiDmO1
-unzip whatsapp-web-project-20231019T080437Z-001.zip
-
-# التنقل إلى المجلد الجديد
-cd whatsapp-web-project
-
-# تحميل الملف الثاني
-gdown https://drive.google.com/uc?id=1QZIo7pd2d67Qb59j33cn3URAM-i5fRxE
-
-# تشغيل السكريبت
-bash i.sh
-
-# تثبيت حزم npm
-npm install qrcode-terminal whatsapp-web.js translate-google
-
-# تشغيل التطبيق
-node index.js "هل هناك خطأ؟"
+wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
+./ngrok authtoken 2Q62sciZHvRunnnnVbZwLWJkND3_KJpgXdqYuzmMRnDf9VEt
+./ngrok tcp 4000
